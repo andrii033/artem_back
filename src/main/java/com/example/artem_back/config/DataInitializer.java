d404f75f92e57850e6b5cb66947ac23f4711fbbf
Andrii <ataran033@gmail.com>
2025-07-20 13:53:16 +0200

    add DataInitializer.java

---
+++ src/main/java/com/example/artem_back/config/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.example.artem_back.config;
+
+import com.example.artem_back.model.User;
+import com.example.artem_back.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public ApplicationRunner initAdmin() {
+        return args -> {
+            String adminUsername = "admin";
+            if (userRepository.findByUsername(adminUsername).isEmpty()) {
+                User admin = new User();
+                admin.setUsername(adminUsername);
+                admin.setPassword(passwordEncoder.encode("admin"));
+                admin.setRoles(List.of("ADMIN"));
+
+                userRepository.save(admin);
+                System.out.println("🛡️  Admin user created: username=admin, password=admin");
+            }
+        };
+    }
+}
